09fd74e701de82d10a910ff2546540449d4dd0aa
S <s>
2014-10-06 00:31:05 -0700

    {"type":"TI", "runs":"1"}

--- Target.java
+++ Target.java
@@ -16,6 +16,11 @@ public class Target extends GraphicsProgram {
 		makeMiddleCircle();
 		makeOuterCircle();
 	}
+	/* Creates the biggest, innermost circle and colors it red.
+	 * Precondition: The window is empty
+	 * Postcondition: There is a red circle in the middle of the
+	 * window.
+	 */
 	private void makeInnerCircle(){
 		double x1 = getWidth()/2 - 72;
 		double y1 = getHeight()/2 - 72;
@@ -24,6 +29,11 @@ public class Target extends GraphicsProgram {
 		oval.setColor(Color.RED);
 		add(oval);
 	}
+	/* Creates the middle circle and colors it white.
+	 * Precondition: The window contains the biggest red circle.
+	 * Postcondition: There is a white circle centered in the red
+	 * circle.
+	 */
 	private void makeMiddleCircle(){
 		double x1 = getWidth()/2 - (72*0.65);
 		double y1 = getHeight()/2 - (72*0.65);
@@ -32,6 +42,13 @@ public class Target extends GraphicsProgram {
 		oval.setColor(Color.WHITE);
 		add(oval);
 	}
+	/* Creates the smallest, outermost circle and colors it red.
+	 * Precondition: The window contains both the largest red
+	 * circle and the middle white circle.
+	 * Postcondition: There is a smaller red circle in the white
+	 * circle and the target is complete at the center of the
+	 * window.
+	 */
 	private void makeOuterCircle(){
 		double x1 = getWidth()/2 - (72*0.3);
 		double y1 = getHeight()/2 - (72*0.3);
