40efa1b0481857d8a81a6aa819c9a58094baea73
S <s>
2014-10-11 20:46:23 -0700

    {"type":"TI", "runs":"1"}

--- FindRange.java
+++ FindRange.java
@@ -12,14 +12,14 @@ public class FindRange extends ConsoleProgram {
 	private static final double SENTINEL = 0;
 	public void run() {
 		println("This program finds the largest and the smallest numbers.");
-		int x = readInt("?: ");
-		int smallest = x;
-		int largest = x;
+		double x = readDouble("?: ");
+		double smallest = x;
+		double largest = x;
 		if( x == SENTINEL){
 			println("no values have been entered");
 		} else {
-			while(readInt("?: ") != SENTINEL){
-				if(x < smallest){
+			while(readDouble("?: ") != SENTINEL){
+				if(x < smallest || x<0 && x<smallest){
 					smallest = x;
 				}
 				if(x>largest){
