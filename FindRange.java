851f969c975d71ebe7f9dc1c6ee1d87f8abaa0d0
S <s>
2014-10-28 12:55:11 -0700

    {"type":"TI", "runs":"0"}

--- FindRange.java
+++ FindRange.java
@@ -10,7 +10,26 @@ import acm.program.*;
 
 public class FindRange extends ConsoleProgram {
 	private static final double SENTINEL = 0;
-	public void run() {
+	public void run(){
+		println("This program finds the two largest integers in a list." +
+				"Enter valutes, one per line, using a 0 to signal the end of" +
+				"the list");
+		double x = readDouble("? ");
+		double largest = x;
+		double secondLargest = x;
+		while(x != SENTINEL){
+			if(x > largest){
+				secondLargest = largest;
+				largest = x;
+			} else if(x > secondLargest){
+				secondLargest = x;
+			}
+			x = readDouble("? ");
+		}
+		println("The largest value is"+largest+"");
+		println("The second largest value is"+secondLargest+"");
+	}
+	/*public void run() {
 		println("This program finds the largest and the smallest numbers.");
 		if( readDouble("?: ") == SENTINEL){
 			println("no values have been entered");
@@ -31,6 +50,6 @@ public class FindRange extends ConsoleProgram {
 			println("smallest: "+smallest+"");
 			println("largest:"+largest+"");
 		}
-	}
+	}*/
 }
 
